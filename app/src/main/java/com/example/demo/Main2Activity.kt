f8f3261d90a401af6d28609f24bfe7d8b7ca3e33
Apple <apple@123-macbook-pro.lan>
2020-03-03 15:16:46 +0530

    Scrollview

--- app/src/main/java/com/example/demo/Main2Activity.kt
+++ app/src/main/java/com/example/demo/Main2Activity.kt
@@ -10,12 +10,6 @@ class Main2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-
-
 
     }
 }
